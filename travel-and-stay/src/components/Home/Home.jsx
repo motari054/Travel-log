9af451c70fd0518e1c08211eda5013a9d1b266ee
sam <samuelmotari54@gmail.com>
2024-08-27 15:59:28 +0300

    responsivity, Aos

--- travel-and-stay/src/components/Home/Home.jsx
+++ travel-and-stay/src/components/Home/Home.jsx
@@ -1,29 +1,37 @@
 import './home.css'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
+
 export function Home(){
+    useEffect(()=>{
+        Aos.init({duration: 2000})
+    }, [])
+
     return (
         <>
         <section className="home">
             <div className="secContainer container">
                 <div className="homeText">
-                    <h1 className="title">Plan your trip with Travel Dot</h1>
-                    <p className="subTitle">
+                    <h1 data-aos="fade-up" className="title">Plan your trip with Travel Dot</h1>
+                    <p data-aos="fade-up" data-aos-duration="2500" className="subTitle">
                         Travel to your favorite city with respect of the environment
                     </p>
 
-                    <button className="btn"><a href="#">Explore Now</a></button>
+                    <button data-aos="fade-up" data-aos-duration="3000" className="btn"><a href="#">Explore Now</a></button>
 
                     <div className="homeCard grid">
-                        <div className="locationDiv">
+                        <div data-aos="fade-right" data-aos-duration="1500" className="locationDiv">
                             <label htmlFor="location">Location</label>
                             <input type="text" name="" placeholder='Dream Destination' id="" />
                         </div>
 
-                        <div className="distDiv">
+                        <div data-aos="fade-right" data-aos-duration="2000" className="distDiv">
                             <label htmlFor="distance">Location</label>
                             <input type="text" name="" placeholder='11/meters' id="" />
                         </div>
 
-                        <div className="priceDiv">
+                        <div data-aos="fade-right" data-aos-duration="2500" className="priceDiv">
                             <label htmlFor="price">Location</label>
                             <input type="text" name="" placeholder='$140-$500' id="" />
                         </div>
